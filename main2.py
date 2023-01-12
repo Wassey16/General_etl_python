665c52cae0735e5332f4ed76137b9f1712388e10
Abdul Wassey <abdul.wassey@datumbrain.com>
2023-01-12 17:25:33 +0500

    modified main2.py * changed code to also store lemma in word table

--- main2.py
+++ main2.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import psycopg2
 
-column_names = ['word', 'PoS', 'l1', 'l2', 'l3', 'l4', 'l5', 'l6']
+column_names = ['word', 'PoS', 'l1', 'lemma', 'l3', 'l4', 'l5', 'l6']
 data = pd.read_csv('alaa.vert', sep='\t', names=column_names, skiprows=4)
-df=data[['word','PoS']]
+df=data[['word','lemma','PoS']]
 df = df.dropna(subset=['PoS'])
 x = df.drop_duplicates(subset='word', keep='first')
 
@@ -16,11 +16,11 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Create table
-cur.execute("CREATE TABLE word(id serial not null primary key, word varchar(35) not null, pos varchar);")
+cur.execute("CREATE TABLE word(id serial not null primary key, word varchar(35) not null, lemma varchar(35), pos varchar);")
 
 # Write the DataFrame to the table
 for _, row in x.iterrows():
-    cur.execute("INSERT INTO word(word, pos) VALUES (%s, %s)", (row['word'], row['PoS']))
+    cur.execute("INSERT INTO word(word,lemma, pos) VALUES (%s, %s, %s)", (row['word'],row['lemma'], row['PoS']))
 
 # Save the changes
 conn.commit()
